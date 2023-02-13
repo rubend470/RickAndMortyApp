77c608cbce35cf652d7d1880d57613379d8dce64
Ruben Duarte <rubenduarte470@gmail.com>
2023-02-13 16:36:23 -0400

    initial commit

---
+++ Rick&MortyApp/Rick_MortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyAppApp.swift
+//  Rick&MortyApp
+//
+//  Created by Ruben Duarte on 13/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
